7ce00d5ab85f50ffc3cec5053dea266378b10abb
kipo <kipo.vip@mail.ru>
2024-01-26 23:01:55 +0500

    Initial commit

---
+++ MySpringBootH2DBase-master/src/main/java/ru/frolov/MyRestSpringBootAppH2DB/MyRestSpringBootAppH2DbApplication.java
@@ -0,0 +1,11 @@
+package ru.frolov.MyRestSpringBootAppH2DB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyRestSpringBootAppH2DbApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(MyRestSpringBootAppH2DbApplication.class, args);
+	}
+}
